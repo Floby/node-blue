326f0ccbea762d297128db874805c5e0e6ea00d6
Florent Jaby <florent.jaby@gmail.com>
2013-03-29 12:24:55 +0100

    add test for writing streams

---
+++ test/test-25-data-stream.js
@@ -0,0 +1,23 @@
+var Sink = require('./sink');
+var blue = require('../');
+
+exports.testDataStream = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/data.tpl');
+    var pt = new require('stream').PassThrough();
+    t.data({
+        str: pt
+    })
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), 'hello world!', "sink data should be identical");
+        test.done();
+    });
+
+    setTimeout(function() {
+        pt.end('hello world!');
+    }, 50);
+}
