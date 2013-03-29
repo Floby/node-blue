6136a084bc064996a982866a17dfa8284fe2e8d0
Florent Jaby <florent.jaby@gmail.com>
2013-03-29 12:33:20 +0100

    refactor make_print to use streams always

--- test/test-00-onefile.js
+++ test/test-00-onefile.js
@@ -4,6 +4,10 @@ var blue = require('../');
 exports.testOneFile = function(test) {
     var sink = Sink();
     var t = new blue.Template(__dirname + '/templates/hw.tpl');
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
     t.pipe(sink).on('data', function(data) {
         test.equal(data.trim(), 'hello world!', "sink data should be identical");
         test.done();
---
+++ test/test-30-filter.js
@@ -0,0 +1,18 @@
+var Sink = require('./sink');
+var blue = require('../');
+
+//exports.testFilterStream = function(test) {
+    //var sink = Sink();
+    //var t = new blue.Template(__dirname + '/templates/filter.tpl');
+    //t.data({
+        //str: "hello world!"
+    //})
+    //t.on('error', function(err) {
+        //test.fail(err.message);
+        //test.done();
+    //});
+    //t.pipe(sink).on('data', function(data) {
+        //test.equal(data.trim(), 'hello world!', "sink data should be identical");
+        //test.done();
+    //});
+//}
