f98eb65280f0bc73147c4942466754cca4ae5bb6
Florent Jaby <florent.jaby@gmail.com>
2013-03-29 15:38:09 +0100

    test filters on streams

--- test/test-30-filter.js
+++ test/test-30-filter.js
@@ -1,18 +1,34 @@
 var Sink = require('./sink');
 var blue = require('../');
 
-//exports.testFilterStream = function(test) {
-    //var sink = Sink();
-    //var t = new blue.Template(__dirname + '/templates/filter.tpl');
-    //t.data({
-        //str: "hello world!"
-    //})
-    //t.on('error', function(err) {
-        //test.fail(err.message);
-        //test.done();
-    //});
-    //t.pipe(sink).on('data', function(data) {
-        //test.equal(data.trim(), 'hello world!', "sink data should be identical");
-        //test.done();
-    //});
-//}
+exports.testFilterData = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/filter.tpl');
+    t.data({
+        str: "hello world!"
+    })
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), 'HELLO WORLD!', "sink data should be identical");
+        test.done();
+    });
+}
+
+exports.testFilterStream = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/filter.tpl');
+    t.data({
+        str: new blue.Template(__dirname + '/templates/hw.tpl')
+    })
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), 'HELLO WORLD!', "sink data should be identical");
+        test.done();
+    });
+}
