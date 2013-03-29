7adeda2504dbd6ceeee00adff195584f63bb5705
Florent Jaby <florent.jaby@gmail.com>
2013-03-29 15:50:04 +0100

    can use transform streams as filters

---
+++ test/templates/filter.gunzip.tpl
@@ -0,0 +1,2 @@
+<%= this.str | gunzip %>
+
--- test/test-30-filter.js
+++ test/test-30-filter.js
@@ -32,3 +32,24 @@ exports.testFilterStream = function(test) {
         test.done();
     });
 }
+
+
+exports.testFilterStreamWithTransform = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/filter.gunzip.tpl');
+    var source = require('zlib').createGzip()
+    t.data({
+        str: source
+    })
+    blue.registerFilter('gunzip', require('zlib').Gunzip);
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), 'hello world!', "sink data should be identical");
+        test.done();
+    });
+
+    source.end('hello world!');
+}
