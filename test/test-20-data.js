f1e5a5f1ee0f2bf64f85aaae2adf9a37d5fb0449
Florent Jaby <florent.jaby@gmail.com>
2013-03-29 12:15:04 +0100

    add tests for data

--- test/test-20-data.js
+++ test/test-20-data.js
@@ -1,12 +1,45 @@
 var Sink = require('./sink');
 var blue = require('../');
 
-exports.testOneFile = function(test) {
+exports.testDataString = function(test) {
     var sink = Sink();
     var t = new blue.Template(__dirname + '/templates/data.tpl');
     t.data({
         str: "hello world!"
     })
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), 'hello world!', "sink data should be identical");
+        test.done();
+    });
+}
+
+exports.testDataUndefined = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/data.tpl');
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
+    t.pipe(sink).on('data', function(data) {
+        test.equal(data.trim(), '', "sink data should be identical");
+        test.done();
+    });
+}
+
+exports.testDataAsThis = function(test) {
+    var sink = Sink();
+    var t = new blue.Template(__dirname + '/templates/data.this.tpl');
+    t.data({
+        str: "hello world!"
+    })
+    t.on('error', function(err) {
+        test.fail(err.message);
+        test.done();
+    });
     t.pipe(sink).on('data', function(data) {
         test.equal(data.trim(), 'hello world!', "sink data should be identical");
         test.done();
